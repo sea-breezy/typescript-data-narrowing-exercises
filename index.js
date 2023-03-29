96b261b0dcd3c3417e4abf5049409e75929de80c
Andres Rojas <andres.rojas@thoughtworks.com>
2023-03-29 16:00:22 +0100

    Setup

--- index.js
+++ index.js
@@ -1,27 +1,14 @@
 console.log("Hello world");
-
-
 //Scenario
-
-
-
 //Client
 //Admin
 //Dev
-
 //Some code that is not really using types.
-function displayHomePageText(User: string) {
-    console.log('Hello user')
-
+function displayHomePageText(User) {
+    console.log('Hello user');
 }
-
 //Showcase different ways of type narrowing.
-
 //Union types
-
 //Type predicates
-
 //Flow analysis - how the parser helps you with undefined and reducing the scope
-
-
 displayHomePageText('test');
